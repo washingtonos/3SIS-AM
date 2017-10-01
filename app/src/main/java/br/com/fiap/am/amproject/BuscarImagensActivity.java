54b3d867865c56cfffd204fd135876f69b8cd066
Renan Lima <renan.n.lima@hotmail.com>
2017-10-01 15:35:14 -0300

    Merge branch 'am_3_record' of https://github.com/washingtonos/3SIS-AM into develop
    # Conflicts:
    #	.idea/modules.xml

--- app/src/main/java/br/com/fiap/am/amproject/BuscarImagensActivity.java
+++ app/src/main/java/br/com/fiap/am/amproject/BuscarImagensActivity.java
@@ -7,10 +7,13 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,12 +22,19 @@ import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BuscarImagensActivity extends AppCompatActivity {
 
     static final int TAKE_PICTURE_CODE = 1;
     static final int OPEN_LOCAL_FOLDER=2;
     private static final int MY_PERMISSION_WRITE_EXTERNAL_STORAGE = 1;
+    private String imagePhotoPath;
+    private File image;
+    private String nomeProduto;
+    private String precoProduto;
 
 
     @Override
@@ -36,6 +46,19 @@ public class BuscarImagensActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        //capturar extras de CadastrarVendaNomeProduto E DefinirPrecoVenda
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras!=null){
+
+            nomeProduto = extras.getString("nomeProduto");
+            precoProduto = extras.getString("precoProduto");
+
+
+        }
     }
 
 
@@ -46,9 +69,11 @@ public class BuscarImagensActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(BuscarImagensActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},MY_PERMISSION_WRITE_EXTERNAL_STORAGE);
         }else{
 
-            Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            /*Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-            startActivityForResult(intentTakePicture,TAKE_PICTURE_CODE);
+            startActivityForResult(intentTakePicture,TAKE_PICTURE_CODE);*/
+
+            callCamera();
 
         }
 
@@ -65,9 +90,17 @@ public class BuscarImagensActivity extends AppCompatActivity {
                 if(grantResults.length>0
                         &&grantResults[0]==PackageManager.PERMISSION_GRANTED){
 
-                    Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    /*Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                    startActivityForResult(intentTakePicture,TAKE_PICTURE_CODE);
+                    File file = createImageFromFile();
+
+                    Uri photoUri = FileProvider.getUriForFile(this,"br.com.fiap.am.amproject.fileprovider",file);
+
+                    intentTakePicture.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+
+                    startActivityForResult(intentTakePicture,TAKE_PICTURE_CODE);*/
+
+                    callCamera();
                 }
         }
     }
@@ -76,23 +109,33 @@ public class BuscarImagensActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if(requestCode==TAKE_PICTURE_CODE&&resultCode==RESULT_OK){
-            Bundle extra = data.getExtras();
+
+
+            Intent intent = new Intent(BuscarImagensActivity.this,InformacoesSobreVendaActivity.class);
+            intent.putExtra("classe",this.getLocalClassName());
+            intent.putExtra("path",imagePhotoPath);
+            intent.putExtra("nomeProduto",nomeProduto);
+            intent.putExtra("precoProduto",precoProduto);
+            startActivity(intent);
+
+
+            /*Bundle extra = data.getExtras();
             if(extra!=null){
                 Bitmap bitmap = (Bitmap)extra.get("data");
 
-                Uri tempUri = getImageUri(getApplicationContext(),bitmap);
+                /*Uri tempUri = getImageUri(getApplicationContext(),bitmap);
 
                 File file = new File(getPathFromUri(tempUri));
+                File file = createImageFromFile();
 
+                Uri photoUri = FileProvider.getUriForFile(this,"br.com.fiap.am.amproject.fileprovider",file);
 
                 Intent intent = new Intent(BuscarImagensActivity.this,InformacoesSobreVendaActivity.class);
                 intent.putExtra("classe",this.getLocalClassName());
                 intent.putExtra("path",file.getPath());
                 startActivity(intent);
 
-            //ImageView imageView = (ImageView) findViewById(R.id.image_picture);
-            //imageView.setImageBitmap(bitmap);
-            }
+            }*/
 
             //Metodo para capturar imagem
         }else if(requestCode==OPEN_LOCAL_FOLDER&&resultCode==RESULT_OK){
@@ -128,5 +171,39 @@ public class BuscarImagensActivity extends AppCompatActivity {
         Intent intent = new Intent(BuscarImagensActivity.this,InformacoesSobreVendaActivity.class);
         intent.putExtra("classe",this.getLocalClassName());
         startActivity(intent);
+    }
+
+
+
+    private void callCamera(){
+
+        Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File file = createImageFromFile();
+
+        Uri photoUri = FileProvider.getUriForFile(this,"br.com.fiap.am.amproject.fileprovider",file);
+
+        intentTakePicture.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+
+        startActivityForResult(intentTakePicture,TAKE_PICTURE_CODE);
+    }
+
+    private File createImageFromFile(){
+        try {
+            String timestamp = new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timestamp + "_";
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+            image = File.createTempFile(imageFileName,".jpg",storageDir);
+
+            imagePhotoPath = image.getAbsolutePath();
+
+            return image;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
